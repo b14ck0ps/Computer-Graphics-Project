715381c8e4fdad06624e82c49d4622a595e3b08f
Azran Hossain <mr69751@gmail.com>
2021-08-14 14:08:20 +0600

    DrawCircle function added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <cstdio>
 #include <GL/gl.h>
 #include <GL/glut.h>
+#include <math.h>
+
+void DrawCircle(float cx, float cy, float r, int num_segments){
+
+		 glBegin(GL_TRIANGLE_FAN);
+		for (int i = 0; i < num_segments; i++)
+		{
+		float theta = 2.0f * 3.1415926f * float(i) / float(num_segments);//get the current angle
+
+		float x = r * cosf(theta);//calculate the x component
+		float y = r * sinf(theta);//calculate the y component
+
+		glVertex2f(x + cx, y + cy);//output vertex
+
+		}
+		glEnd();
+	}
+
 void sky()
 {
     ///---------------------Ajran-----------------------///
