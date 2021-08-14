f1b9e0c1f179b99dfd0737d60cee09e31d85f6fd
Azran Hossain <mr69751@gmail.com>
2021-08-15 00:27:42 +0600

    Stars Added

--- main.cpp
+++ main.cpp
@@ -72,7 +72,7 @@ void moon()
 {
     glPushMatrix();
     glTranslated(0, -200, 0);
-    glColor3f(3.0, 1.0, 0.5);
+    glColor3ub(251, 163, 31);
     DrawCircle(972, 402, 30, 2000);
     glColor3ub(0, 127, 179);
     DrawCircle(960, 405, 30, 2000);
@@ -109,7 +109,78 @@ struct cloud
         glEnd();
     }
 };
+struct star
+{
+    void drawstars()
+    {
+        glColor3ub(251, 163, 31);
+        glBegin(GL_TRIANGLE_FAN);
 
+        glVertex2i(535, 608);
+        glVertex2i(502, 597);
+        glVertex2i(532, 592);
+
+        glVertex2i(541, 560);
+        glVertex2i(552, 590);
+
+        glVertex2i(593, 599);
+        glVertex2i(553, 607);
+
+        glVertex2i(544, 647);
+        glVertex2i(532, 592);
+
+        glEnd();
+    }
+};
+void stars()
+{
+    star s;
+    //stars 1
+    glPushMatrix();
+    glScalef(0.3, 0.3, 0.0);
+    glTranslated(2000, 1000, 0);
+    s.drawstars();
+    glPopMatrix();
+    //stars 2
+    glPushMatrix();
+    glScalef(0.3, 0.3, 0.0);
+    glTranslated(2200, 800, 0);
+    s.drawstars();
+    glPopMatrix();
+    //stars 3
+    glPushMatrix();
+    glScalef(0.1, 0.1, 0.0);
+    glTranslated(8000, 4200, 0);
+    s.drawstars();
+    glPopMatrix();
+    //stars 4
+    glPushMatrix();
+    glScalef(0.1, 0.1, 0.0);
+    glTranslated(9000, 4000, 0);
+    s.drawstars();
+    glPopMatrix();
+    //stars 5
+    glPushMatrix();
+    glScalef(0.1, 0.1, 0.0);
+    glTranslated(8700, 3850, 0);
+    s.drawstars();
+    glPopMatrix();
+    //stars 6
+    glPushMatrix();
+    glScalef(0.1, 0.1, 0.0);
+    glTranslated(8900, 3600, 0);
+    s.drawstars();
+    glPopMatrix();
+    //point stars
+    glPushMatrix();
+    glBegin(GL_POINTS);
+    glColor3ub(251, 163, 31);
+    glVertex2i(947,465);
+    glVertex2i(901,441);
+    glVertex2i(912,421);
+    glEnd();
+    glPopMatrix();
+}
 void DayCloud()
 {
     cloud c;
@@ -9227,6 +9298,7 @@ void Night()
     Nightsky();
     MoonRise();
     moonset();
+    stars();
     moveCloud();
     BgBuildingNightTime();
     RoadFielNight();
@@ -9245,7 +9317,6 @@ void myDisplay(void)
     {
         Night();
     }
-
     glFlush();
     glutPostRedisplay();
     glutSwapBuffers();
